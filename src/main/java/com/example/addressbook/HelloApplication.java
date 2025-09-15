552c1f1691e320f8d527f8fd6aa0e28412a3b9cc
Jason <mingqingxu911@gmail.com>
2025-09-16 02:03:49 +1000

    Update HelloApplication.java

--- src/main/java/com/example/addressbook/HelloApplication.java
+++ src/main/java/com/example/addressbook/HelloApplication.java
@@ -4,15 +4,19 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
-public class  HelloApplication extends Application {
-    // Constants defining the window title and size
-    public static final String TITLE = "Address Book";
-    public static final int WIDTH = 640;
-    public static final int HEIGHT = 360;
 
+public class HelloApplication extends Application {
+
+
+    public static final String TITLE = "Address Book";
+
+    public static final int WIDTH = 640;
+
+
+    public static final int HEIGHT = 360;
+    
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
@@ -21,6 +25,7 @@ public class  HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
     }
+
 
     public static void main(String[] args) {
         launch();
